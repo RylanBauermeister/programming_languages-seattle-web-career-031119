fcb0d8fe254834ff4bd96ed775d54f8bc15ba491
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-17 18:19:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,12 @@
+require pry
+
 def reformat_languages(languages)
   # your code here
   result = {}
   languages.each do |style, langs|
     langs.each do |lang, types|
       if(result[lang])
+        binding.pry
         result[:style] << style
       else 
         result[lang] = {
