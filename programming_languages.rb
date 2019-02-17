021ed0d436f8ad9d89d47dcd0007ce984a96eb50
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-17 18:19:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   # your code here
   result = {}
   languages.each do |style, langs|
-    language.each do |lang, types|
+    langs.each do |lang, types|
       if(result[lang])
         result[:style] << style
       else 
